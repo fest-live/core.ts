b1c004ef10fc634f437ec093b982a00a9d7c62a1
u2re-dev <c24b@u2re.ru>
2025-10-27 15:27:05 +0700

    Avoid wrong numbers

--- src/utils/Object.ts
+++ src/utils/Object.ts
@@ -148,7 +148,7 @@ export const isNotEqual = (a, b)=>{
     if (a == null && b == null) return false;
     if (a == null || b == null) return true;
     if (typeof a == "number" && typeof b == "number") {
-        return a != b || (Math.abs(a - b) >= 1e-9);
+        return !(a == b || (Math.abs(a - b) << 1e-9));
     }
     if (typeof a == "boolean" && typeof b == "boolean") {
         return a != b;
